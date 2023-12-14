e58e00b94a8b926221c8552b98f45ad015e78bcb
liamjosephnolan <153128186+liamjosephnolan@users.noreply.github.com>
2023-12-14 15:31:31 +0100

    Update Lab1.m

--- Lab1.m
+++ Lab1.m
@@ -21,23 +21,32 @@ height = Data(:,1)';
 voltage = Data(:,2)';
 
 % Plot measured data
-plot(voltage, height, '*')
-xlabel("Voltage (V)")
-ylabel("Height (Cm)")
+plot(height, voltage, '*')
+ylabel("Voltage (V)")
+xlabel("Height (Cm)")
 hold on
 
 % Fit Data to polynomial
-p = polyfit(voltage, height,  3);
+p = polyfit(height, voltage,  5);
 
 % Generate a callable function based on voltage
-fit_function = @(V) polyval(p, V);
+fit_function = @(h) polyval(p, h);
 
 % Plot fitted function
-V_values = linspace(min(voltage), max(voltage), 100); % Adjust the number of points as needed
-height_fit = fit_function(V_values);
-plot(V_values, height_fit)
+h_values = linspace(min(height), max(height), 100); % Adjust the number of points as needed
+V_fit = fit_function(h_values);
+plot(h_values, V_fit)
+% Shade the region from height 20 to 30 in light gray
+h_shade = [20, 30];
+V_shade = [.8, 2.2];
 
-legend("Data Values","Fitted Function",location = "southeast")
+% Fill the shaded region with a light gray color
+fill([min(h_shade), min(h_shade), max(h_shade), max(h_shade)], [min(voltage), max(voltage), max(voltage), min(voltage)], 'k', 'FaceAlpha', 0.1);
 
+
+
+
+% Label the shaded region
+legend("Data Values","Fitted Function","linear region", location = "southeast")
 
 
